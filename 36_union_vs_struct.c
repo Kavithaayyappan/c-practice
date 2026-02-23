34363d5d836a8448e9c454f92c0a395b70044e47
Kavithaayyappan <220801100@rajalakshmi.edu.in>
2026-02-23 13:04:03 +0530

    Delete 36_union_vs_struct.c

--- 36_union_vs_struct.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-union Data{ int i; float f; char str[20]; };
-struct DataS{ int i; float f; char str[20]; };
-int main(){
-    union Data u; struct DataS s;
-    printf("%lu %lu
-",sizeof(u),sizeof(s));
-    return 0;
-}
